6f1a3253b8e94c0871b8bf9ff27cb81aa9928210
PMetcalf <paulmetcalf@hotmail.com>
2020-06-25 09:32:08 +0100

    Added load data

---
+++ source/data/load_data.py
@@ -0,0 +1,22 @@
+'''
+Load_Data
+
+This file supports loading project data into memory.
+
+'''
+
+def load_motor_data():
+    """Load Dataset
+    ======================================
+    Loads dataset from user-specified directory.
+    
+    Args:
+        None.
+        
+    Returns:
+        dataframe (dataframe) - Dataframe loaded with data from csv.
+    """
+
+    file_string = r'C:\Users\ASUS-PC\OneDrive\Cloudforest Technologies\M. Projects\Yellow Cuckoo\pmsm_temperature_data.csv'
+    
+    return pd.read_csv(file_string)
