27a88a568025d209726da98d879d3119f09c39ff
PMetcalf <paulmetcalf@hotmail.com>
2020-07-14 11:38:28 +0100

    Added keras helpers class

---
+++ src/models/keras_helpers.py
@@ -0,0 +1,14 @@
+'''
+keras_helpers
+
+Helper routines for building & training neural networks using keras API
+
+'''
+
+# Module Importations
+
+def create_multilayer_perceptron(dim):
+    pass
+
+def train_multilayer_perceptron(dataset, target):
+    pass
