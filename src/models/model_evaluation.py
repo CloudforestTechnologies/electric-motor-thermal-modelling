b8e477b8241fab89aaf77c0351c2419232066144
PMetcalf <paulmetcalf@hotmail.com>
2020-07-28 09:22:03 +0100

    Outlined model_evaluation module.

--- src/models/model_evaluation.py
+++ src/models/model_evaluation.py
@@ -6,3 +6,28 @@ Helper routines for evaluating model performance
 '''
 
 # Module Importations
+
+
+def evaluate_model():
+    """
+    Evaluate model using series of metrics.
+    ======================================
+
+    Input:
+
+    Output:
+        
+    """
+    pass
+
+def evaluate_mae():
+    pass
+
+def evaluate_rmse():
+    pass
+
+def evaluate_r2():
+    pass
+
+def print_evaluation():
+    pass
