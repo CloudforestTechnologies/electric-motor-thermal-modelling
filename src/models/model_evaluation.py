890b6d0fc324c6660962760831d979e57ffcba1c
PMetcalf <paulmetcalf@hotmail.com>
2020-07-28 09:32:12 +0100

    evaluate_mae outline

--- src/models/model_evaluation.py
+++ src/models/model_evaluation.py
@@ -6,9 +6,10 @@ Helper routines for evaluating model performance
 '''
 
 # Module Importations
+from sklearn.metrics import mean_absolute_error
 
 
-def evaluate_model():
+def evaluate_model(model_name, y_true, y_pred):
     """
     Evaluate model using series of metrics.
     ======================================
@@ -18,10 +19,13 @@ def evaluate_model():
     Output:
         
     """
-    pass
+    mae_eval = evaluate_mae(y_true, y_pred)
 
-def evaluate_mae():
-    pass
+def evaluate_mae(y_true, y_pred):
+
+    mae_eval = mean_absolute_error(y_true, y_pred)
+    
+    return mae_eval
 
 def evaluate_rmse():
     pass
