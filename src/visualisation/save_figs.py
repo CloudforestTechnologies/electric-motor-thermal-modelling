d7c999d4db2367a1e8dd5ab7815e65e8308aeafa
PMetcalf <paulmetcalf@hotmail.com>
2020-08-04 09:50:01 +0100

    save figs wip

--- src/visualisation/save_figs.py
+++ src/visualisation/save_figs.py
@@ -22,14 +22,14 @@ def generate_fig_save_string(filename):
     """
 
     # Save files to project
-    filedirectory = r'C:\Developer\electric_motor_thermal_modelling\Reports\Figures'
+    filedirectory = r'C:/Developer/electric_motor_thermal_modelling/Reports/Figures'
 
     # Retrieve timestamp
     timestamp = datetime.now()
     timestamp_str = timestamp.strftime('%Y-%m-%d %H:%M:%S')
 
     # Create filepath
-    filepath = filedirectory + '\\' + 'YC' + filename + '_' + timestamp_str + '.png'
+    filepath = filedirectory + '/' + 'YC_' + filename + '_' + timestamp_str + '.png'
 
     # Return save string
     #save_string = save_directory + '\\' + filename
