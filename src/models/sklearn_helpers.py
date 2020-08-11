c2280519bf0071d1725703ec71371658ca989373
PMetcalf <paulmetcalf@hotmail.com>
2020-08-11 10:14:59 +0100

    name model / save model routine outlines.

--- src/models/sklearn_helpers.py
+++ src/models/sklearn_helpers.py
@@ -9,3 +9,32 @@ Helper routines for working with sklearn models
 import time
 
 project_code = 'YC'
+
+def name_model(model_type):
+    """
+    Returns a time-stamped model name.
+    ======================================
+
+    Input:
+        model_type (string) - Type of model.
+
+    Output:
+        name (string) - Model name.
+    """
+
+    pass
+
+def save_model():
+    """
+    Save model to Models folder using described name.
+    ======================================
+
+    Input:
+        model () - The model to be saved.
+        name (string) - Model unique identifier
+
+    Output:
+        None.
+    """
+
+    pass
